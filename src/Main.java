9c290773d1a7c3ad8788266faf88b07830a36421
2523350 <ememejia2106@gmail.com>
2025-11-19 17:09:07 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Boolean f=true;
+        boolean f=true;
+        boolean user=true;
+        boolean passw=true;
         while (f){
             System.out.println("Menu\n1-Logon/Create Account 2-Login/Access Account 3-Exit. Enter choice: ");
             int choice =in.nextInt();
             switch (choice){
                 case 1:
-                    System.out.println("Choose the user name: ");
-                    String username=in.next();
                     try {
-                        while (!username.matches("[a-zA-Z]")) {
-                            throw new UserCheck();
+                        while (user) {
+                            System.out.println("Choose the user name: ");
+                            String username=in.next();
+                            if(!username.matches("[a-zA-Z]")){
+                                throw new UserCheck();
+                            }
+                            else{
+                                user=false;
+                            }
                         }
-
                     } catch (UserCheck u) {
                         System.out.println(u);
-                        System.out.println("Choose the user name: ");
-                        username = in.next();
                     }
-                    System.out.println("choose password: ");
-                    String pass=in.next();
                     try {
-                        if(!pass.matches("[a-zA-Z0-9]")) {
-                            throw new PassCheck();
+                        while (passw){
+                            System.out.println("choose password: ");
+                            String pass=in.next();
+                            if(!pass.matches("[a-zA-Z0-9]")) {
+                                throw new PassCheck();
+                            }
+                            else {
+                                passw=false;
+                            }
                         }
                     }catch (PassCheck p){
                         System.out.println(p);

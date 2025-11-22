044127826ed207957b91e9c3a901e298dc0e090f
Emelie <ememejia2106@gmail.com>
2025-11-22 18:55:10 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        ArrayList<Profile>  profiles = new ArrayList<>();
+        String username, pass;
         boolean f=true;
         boolean user=true;
         boolean passw=true;
@@ -14,43 +17,63 @@ public class Main {
                     try {
                         while (user) {
                             System.out.println("Choose the user name: ");
-                            String username=in.next();
+                            username=in.next();
                             if(!username.matches("[a-zA-Z]")){
                                 throw new UserCheck();
                             }
                             else{
                                 user=false;
+                                try {
+                                    while (passw){
+                                        System.out.println("choose password: ");
+                                        pass=in.next();
+                                        if(!pass.matches("[a-zA-Z0-9]")) {
+                                            throw new PassCheck();
+                                        }
+                                        else {
+                                            passw=false;
+                                            System.out.println("Enter the first hint: ");
+                                            String hint1= in.next();
+                                            System.out.println("Enter the answer to the first hint: ");
+                                            String hint1Answer= in.next();
+                                            System.out.println("Enter the second hint: ");
+                                            String hint2= in.next();
+                                            System.out.println("Enter the answer to the second hint: ");
+                                            String hint2Answer= in.next();
+                                            profiles.add(new Profile(username,pass,hint1,hint1Answer,hint2,hint2Answer));
+                                        }
+                                    }
+                                }catch (PassCheck p){
+                                    System.out.println(p);
+                                }
                             }
                         }
                     } catch (UserCheck u) {
                         System.out.println(u);
                     }
-                    try {
-                        while (passw){
-                            System.out.println("choose password: ");
-                            String pass=in.next();
-                            if(!pass.matches("[a-zA-Z0-9]")) {
-                                throw new PassCheck();
-                            }
-                            else {
-                                passw=false;
-                            }
-                        }
-                    }catch (PassCheck p){
-                        System.out.println(p);
-                    }
-                    System.out.println("Enter the first hint: ");
-                    System.out.println("Enter the answer to the first hint: ");
-                    System.out.println("Enter the second hint: ");
-                    System.out.println("Enter the answer to the second hint: ");
                     break;
                 case 2:
-                    System.out.println("Enter password: ");
+                    System.out.println("Enter useraname: ");
                     String um=in.next();
-                    try {
-
-                    }catch (Exception e){
-
+                    for(Profile p:profiles){
+                        if(um.equals(p.username)){
+                            System.out.println("Enter password: ");
+                            String password=in.next();
+                            if(password.equals(p.password)){
+                                System.out.println("Hint: "+p.hint1);
+                                System.out.println("Enter answer to the first hint: ");
+                                String ans=in.next();
+                                System.out.println("Hint: "+p.hint2);
+                                System.out.println("Enter answer to the second hint: ");
+                                String ans2=in.next();
+                                if(ans.equals(p.ans_hint1) && ans2.equals(p.ans_hint2)){
+                                    System.out.println("Enjoy Java");
+                                }
+                                else {
+                                    System.out.println("Wrong answer to security answer");
+                                }
+                            }
+                        }
                     }
                     break;
                 case 3:
--- src/Profile.java
+++ src/Profile.java
@@ -5,4 +5,13 @@ public class Profile {
     String ans_hint1;
     String hint2;
     String ans_hint2;
+
+    public Profile(String username, String password, String hint1, String ans_hint1, String hint2, String ans_hint2) {
+        this.username = username;
+        this.password = password;
+        this.hint1 = hint1;
+        this.ans_hint1 = ans_hint1;
+        this.hint2 = hint2;
+        this.ans_hint2 = ans_hint2;
+    }
 }
